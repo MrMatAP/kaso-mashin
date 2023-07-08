4bb5d6c536be2aefb3f83892f61ee1df25eb3ffa
Mathieu Imfeld <imfeldma@gmail.com>
2023-07-08 10:04:25 +0200

    Removed Ansible

--- src/kaso_mashin/model/__init__.py
+++ src/kaso_mashin/model/__init__.py
@@ -7,9 +7,3 @@ from .network_model import NetworkKind, NetworkModel
 from .bootstrap_model import BootstrapKind, CIVendorData, CIMetaData, CIUserData, CINetworkConfig
 from .instance_model import InstanceModel
 from .vm_script_model import VMScriptModel
-
-from .ansible import AnsibleInventory, AnsibleCfg, AnsiblePlaybook
-
-
-
-
