c7d5f05774a25cbd4c991a36d4c94af4f7903734
Mathieu Imfeld <imfeldma@gmail.com>
2023-07-16 16:41:22 +0200

    Introduced an api client with some better error handling

--- src/kaso_mashin/commands/abstract_commands.py
+++ src/kaso_mashin/commands/abstract_commands.py
@@ -1,12 +1,18 @@
 import abc
+import typing
 import argparse
+import httpx
+import rich.table
+import rich.box
 
+from kaso_mashin import console
 from kaso_mashin.config import Config
 from kaso_mashin.db import DB
 from kaso_mashin.runtime import Runtime
 from kaso_mashin.controllers import (
     BootstrapController, DiskController, IdentityController, ImageController, InstanceController,
     NetworkController, PhoneHomeController)
+from kaso_mashin.model import ExceptionSchema
 
 
 class AbstractCommands(abc.ABC):
@@ -20,6 +26,27 @@ class AbstractCommands(abc.ABC):
     @abc.abstractmethod
     def register_commands(self, parser: argparse.ArgumentParser):
         pass
+
+    def api_client(self,
+                   uri: str,
+                   body: typing.Dict | typing.List = None,
+                   method: str = 'GET',
+                   expected_status: typing.List = [200],
+                   fallback_msg: str = 'Something bad and unknown happened...'):
+        resp = httpx.request(url=f'{self.server_url}{uri}',
+                             method=method,
+                             json=body)
+        if resp.status_code in expected_status:
+            return resp
+        table = rich.table.Table(title='ERROR', box=rich.box.ROUNDED, show_header=False)
+        table.add_row('[red]Status:', str(resp.status_code))
+        try:
+            ex = ExceptionSchema.model_validate_json(resp.content)
+            table.add_row('[red]Message:', ex.msg)
+        except ValueError:
+            table.add_row('[red]Message:', fallback_msg)
+        console.print(table)
+        return None
 
     @property
     def config(self) -> Config:
