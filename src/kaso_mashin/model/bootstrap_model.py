d282faf13eac35d7a672f6c8ead662cb5335490a
Mathieu Imfeld <imfeldma@gmail.com>
2023-07-31 17:24:50 +0200

    Reshuffled contents for clarity

--- src/kaso_mashin/model/bootstrap_model.py
+++ src/kaso_mashin/model/bootstrap_model.py
@@ -7,7 +7,9 @@ try:
 except ImportError:
     from yaml import Dumper                 # pylint: disable=unused-import
 
-from .renderable import Renderable
+from kaso_mashin.renderable import Renderable
+from .identity_model import IdentityKind
+from .instance_model import InstanceModel
 
 
 class BootstrapKind(enum.Enum):
@@ -15,6 +17,18 @@ class BootstrapKind(enum.Enum):
     CI_DISK = 'ci-disk'
     IGNITION = 'ignition'
     NONE = 'none'
+
+
+class CIVendorData(Renderable):
+    """
+    Manages cloud-init vendor data
+    """
+
+    def render(self) -> str:
+        return ''
+
+    def __repr__(self):
+        return 'VendorData()'
 
 
 class CIMetaData(Renderable):
@@ -39,6 +53,86 @@ class CIMetaData(Renderable):
             'instance-id': f'{self.instance_id}/{self.hostname}',
             'local-hostname': self.hostname
         })
+
+
+class CIUserData(Renderable):
+    """
+    Manages cloud-init userdata
+    """
+
+    def __init__(self, phone_home_url: str, model: InstanceModel):
+        self._phone_home_url = phone_home_url
+        self._locale = 'en_US'
+        self._timezone = 'Europe/Zurich'
+        self._model = model
+
+    @property
+    def phone_home_url(self) -> str:
+        return self._phone_home_url
+
+    @property
+    def locale(self) -> str:
+        return self._locale
+
+    @locale.setter
+    def locale(self, value: str):
+        self._locale = value
+
+    @property
+    def timezone(self) -> str:
+        return self._timezone
+
+    @timezone.setter
+    def timezone(self, value: str):
+        self._timezone = value
+
+    @property
+    def model(self):
+        return self._model
+
+    def render(self) -> str:
+        userdata = {
+            'growpart': {
+                'mode': 'auto',
+                'devices': ['/']
+            },
+            'locale': self.locale,
+            'timezone': self.timezone,
+            'ssh_pwauth': True,
+            'package_update': True,
+            'package_upgrade': True,
+            'phone_home': {
+                'url': self.phone_home_url,
+                'post': [
+                    'pub_key_rsa',
+                    'instance_id',
+                    'hostname',
+                    'fqdn'
+                ],
+                'tries': 5
+            },
+            'users': []
+        }
+        for identity in self.model.identities:
+            account = {
+                'name': identity.name,
+                'groups': 'users,admin,wheel',
+                'sudo': 'ALL=(ALL) NOPASSWD:ALL'
+            }
+            if identity.gecos:
+                account['gecos'] = identity.gecos
+            if identity.homedir:
+                account['homedir'] = identity.homedir
+            if identity.shell:
+                account['shell'] = identity.shell
+            if identity.kind == IdentityKind.PUBKEY:
+                account['ssh_authorized_keys'] = [f'{identity.pubkey}']
+                account['lock_passwd'] = True
+            else:
+                account['plain_text_passwd'] = identity.passwd
+                account['lock_passwd'] = False
+            userdata['users'].append(account)
+        return '#cloud-config\n---\n' + dump(userdata)
 
 
 class CINetworkConfig(Renderable):
@@ -100,104 +194,4 @@ class CINetworkConfig(Renderable):
         })
 
 
-class CIUserData(Renderable):
-    """
-    Manages cloud-init userdata
-    """
 
-    def __init__(self, phone_home_url: str, pubkey: str):
-        self._pubkey = pubkey
-        self._phone_home_url = phone_home_url
-        self._locale = 'en_US'
-        self._timezone = 'Europe/Zurich'
-        self._admin_password = None
-
-    @property
-    def admin_password(self):
-        return self._admin_password
-
-    @admin_password.setter
-    def admin_password(self, value: str):
-        self._admin_password = value
-
-    @property
-    def pubkey(self) -> str:
-        return self._pubkey
-
-    @pubkey.setter
-    def pubkey(self, value: str):
-        self._pubkey = value
-
-    @property
-    def locale(self) -> str:
-        return self._locale
-
-    @locale.setter
-    def locale(self, value: str):
-        self._locale = value
-
-    @property
-    def timezone(self) -> str:
-        return self._timezone
-
-    @timezone.setter
-    def timezone(self, value: str):
-        self._timezone = value
-
-    @property
-    def phone_home_url(self) -> str:
-        return self._phone_home_url
-
-    def render(self) -> str:
-        return '#cloud-config\n---\n' + dump({
-            'growpart': {
-                'mode': 'auto',
-                'devices': ['/']
-            },
-            'locale': self.locale,
-            'timezone': self.timezone,
-            'ssh_pwauth': True,
-            'users': [
-                {
-                    'name': 'cloudadmin',
-                    'gecos': 'Cloud Admin User',
-                    'groups': 'users,admin,wheel',
-                    'sudo': 'ALL=(ALL) NOPASSWD:ALL',
-                    'shell': '/bin/bash',
-                    'lock_passwd': False,
-                    'plain_text_passwd': 'foobar'
-                },
-                {
-                    'name': 'ansible',
-                    'gecos': 'Ansible Automation User',
-                    'groups': 'users,admin,wheel',
-                    'sudo': 'ALL=(ALL) NOPASSWD:ALL',
-                    'shell': '/bin/bash',
-                    'lock_passwd': True,
-                    'ssh_authorized_keys': [f'{self.pubkey}']
-                }],
-            'package_update': True,
-            'package_upgrade': True,
-            'phone_home': {
-                'url': self._phone_home_url,
-                'post': [
-                    'pub_key_rsa',
-                    'instance_id',
-                    'hostname',
-                    'fqdn'
-                ],
-                'tries': 5
-            }
-        })
-
-
-class CIVendorData(Renderable):
-    """
-    Manages cloud-init vendor data
-    """
-
-    def render(self) -> str:
-        return ''
-
-    def __repr__(self):
-        return 'VendorData()'
