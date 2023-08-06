efc66f01930f9b85bc20f65ea5a451fa525e677c
Mathieu Imfeld <imfeldma@gmail.com>
2023-08-06 12:23:08 +0200

    Make sure to chown the db file

--- src/kaso_mashin/db.py
+++ src/kaso_mashin/db.py
@@ -1,3 +1,5 @@
+import pathlib
+import shutil
 import sqlalchemy
 import sqlalchemy.orm
 
@@ -14,6 +16,8 @@ class DB:
         self._config = config
         self._engine = None
         self._session = None
+        self._path = pathlib.Path(f'{self._config.path}/kaso.sqlite3')
+        self._owning_user = None
 
     def __del__(self):
         """
@@ -23,13 +27,21 @@ class DB:
             self._session.close()
 
     @property
-    def config(self) -> Config:
-        return self._config
+    def path(self) -> pathlib.Path:
+        return self._path
+
+    @property
+    def owning_user(self) -> str:
+        return self._owning_user
+
+    @owning_user.setter
+    def owning_user(self, value: str):
+        self._owning_user = value
 
     @property
     def engine(self) -> sqlalchemy.Engine:
         if not self._engine:
-            self._engine = sqlalchemy.create_engine(f'sqlite:///{self.config.path}/cloud.sqlite3')
+            self._engine = sqlalchemy.create_engine(f'sqlite:///{self.path}')
             Base.metadata.create_all(self._engine)
         return self._engine
 
@@ -37,4 +49,5 @@ class DB:
     def session(self) -> sqlalchemy.orm.Session:
         if not self._session:
             self._session = sqlalchemy.orm.Session(self.engine)
+            shutil.chown(self.path, user=self.owning_user)
         return self._session
