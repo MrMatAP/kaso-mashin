2829047af40389dc3b27a8aa7f5d39353499a0c7
Mathieu Imfeld <imfeldma@gmail.com>
2024-04-13 19:09:24 +0200

    Removed no longer needed controllers

--- src/kaso_mashin/server/controllers/__init__.py
+++ src/kaso_mashin/server/controllers/__init__.py
@@ -1,7 +1,4 @@
 # from .abstract_controller import AbstractController
-# from .network_controller import NetworkController
-# from .os_disk_controller import OsDiskController
-# from .identity_controller import IdentityController
 # from .bootstrap_controller import BootstrapController
 # from .instance_controller import InstanceController
 # from .phone_home_controller import PhoneHomeController
