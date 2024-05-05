1b9c16020e950b6464dfc49a55276ea22b8544ae
Mathieu Imfeld <imfeldma@gmail.com>
2024-05-05 14:18:19 +0200

    Introducing the config store

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,10 +1,19 @@
+<script lang="ts" setup>
+import {onMounted, ref} from 'vue'
+import {ConfigSchema, useConfigStore} from '@/store/config'
+
+const store = useConfigStore()
+const loading = ref(true)
+
+onMounted( () => { store.get().then( () => { loading.value = false } )} )
+</script>
+
+
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <p>Main Content goes here</p>
+      <p>{{ store.config.version }}</p>
     </v-responsive>
   </v-container>
 </template>
-
-<script lang="ts" setup>
-</script>
